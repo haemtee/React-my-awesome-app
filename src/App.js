f6c779d8ef4ac164136a30d0332243174828980a
Hermanto Yap <haemtee@gmail.com>
2020-11-13 11:37:22 +0700

    course

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import "./App.css";
+import Person from "./Person/Person";
+//import { state } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { id: "wer", name: "Andi", age: 20 },
+    { id: "afs", name: "Joni", age: 26 },
+    { id: "ffd", name: "Lucky", age: 29 },
+  ]);
+  const [showPersons, setShowPersons] = useState(false);
+
+  const nameChangedHandler = (event, id) => {
+    const personIndex = persons.findIndex((p) => {
+      return p.id === id;
+    });
+    const person = { ...persons[personIndex] };
+    person.name = event.target.value;
+
+    const namePersons = [...persons];
+    namePersons[personIndex] = person;
+
+    setPersons(namePersons);
+  };
+
+  const togglePersonsHandler = () => {
+    const doesShow = showPersons;
+    setShowPersons(!doesShow);
+  };
+
+  const deletePersonHandler = (personIndex) => {
+    const tempPersons = [...persons];
+    tempPersons.splice(personIndex, 1);
+    setPersons(tempPersons);
+  };
+
+  const styleButton = {
+    backgroundColor: "green",
+    color: "white",
+    font: "inherit",
+    border: "1px solid blue",
+    padding: "8px",
+    cursor: "pointer",
+  };
+
+  let xpersons = null;
+
+  if (showPersons) {
+    xpersons = (
+      <div>
+        {persons.map((person, index) => {
+          return (
+            <Person
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              click={() => deletePersonHandler(index)}
+              changed={(event) => nameChangedHandler(event, person.id)}
+            />
+          );
+        })}
+      </div>
+    );
+    styleButton.backgroundColor = "red";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is my first react App</h1>
+      <button style={styleButton} onClick={togglePersonsHandler}>
+        Show Persons Button
+      </button>
+      {xpersons}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Person.css";
+
+const person = (props) => {
+  return (
+    <div className="Person">
+      <p>
+        Hi, my name is {props.name} and my age is {props.age} years old{" "}
+        {props.children}{" "}
+      </p>
+      <button onClick={props.click}>Delete</button>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
+
+export default person;
